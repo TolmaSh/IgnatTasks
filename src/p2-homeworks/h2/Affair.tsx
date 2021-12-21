f0b9f973ebed358f761c95adbcc277271b16a4e7
TolmaSh <m0v4anua@gmail.com>
2021-12-21 20:40:27 +0200

    feat: style HW2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {AffairType} from "./HW2";
+import s from "./Affairs.module.css"
 
 type AffairPropsType = {
     // key не нужно типизировать
@@ -12,11 +13,14 @@ function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
         props.deleteAffairCallback(props.affair._id)
     }// need to fix
+
+    const priorityType =
+        (props.affair.priority === "low") ? `${s.low}` : (props.affair.priority === "middle") ?  `${s.middle}` : `${s.high}`
     return (
         <div>
             <ul>
-                <li>{props.affair.name}</li>
-                <li>[{props.affair.priority}]</li>
+                <li className={s.taskName}>{props.affair.name}</li>
+                <li className={priorityType}>[{props.affair.priority}]</li>
                 <li>
                     <button onClick={deleteCallback}>X</button>
                 </li>
